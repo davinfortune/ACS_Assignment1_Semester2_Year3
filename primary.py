38465f7ab9b30e420c6caa3308a18a4e87d26e3d
davinfortune <davinfortune@gmail.com>
2021-02-26 16:54:43 +0000

    added main menu

--- primary.py
+++ primary.py
@@ -1,14 +1,35 @@
 import boto3
 
+# AWS API KEYS
+def awsApiKey(accessKeyId,secretAccessKey):
+    # THIS CODE WAS RECIEVED FROM THE BOTO3 DOCUMENTATION : https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html
+    ec2client = boto3.client(
+    'ec2',
+    aws_access_key_id = accessKeyId,
+    aws_secret_access_key = secretAccessKey
+    )
+    print("Your Patched In!")
+
+def mainMenu():
+
+    print("Main Menu")
+    print("---------")
+    print("1. Create A New Instance")
+    print("2. Start/Stop an Instance")
+    print("3. Upload an Image")
+    print("---------")
+    userSelection = input("Your Selection : ")
+    switcher={
+        # 1: createNewInstance(),
+        # 2: startStopper(),
+        # 3: uploadImage()
+    }
+    return switcher.get(userSelection,"Invalid Number Selected")
+    
 userAccessKey = input("Please Enter Your AWS Access Key Id : ")
 userSecretAccessKey = input("Please Enter Your AWS Secret Access Key : ")
-
-
-# AWS API KEYS
-
-def awsApiKey(accessKeyId,secretAcessKey):
-    aws_access_key_id = accessKeyId
-    aws_secret_access_key = secretAcessKey
+awsApiKey(userAccessKey,userSecretAccessKey)
+mainMenu()
 
 
     
