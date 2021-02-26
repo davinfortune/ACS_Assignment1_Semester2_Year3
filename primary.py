463feef99b30931dd0511628e42922b17d4fa136
davinfortune <davinfortune@gmail.com>
2021-02-26 16:30:42 +0000

    first commit

---
+++ primary.py
@@ -0,0 +1,14 @@
+import boto3
+
+userAccessKey = input("Please Enter Your AWS Access Key Id : ")
+userSecretAccessKey = input("Please Enter Your AWS Secret Access Key : ")
+
+
+# AWS API KEYS
+
+def awsApiKey(accessKeyId,secretAcessKey):
+    aws_access_key_id = accessKeyId
+    aws_secret_access_key = secretAcessKey
+
+
+    
