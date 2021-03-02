fe29bb88911d0496ac8a40e1c2e3aae1c1af7fdc
davinfortune <davinfortune@gmail.com>
2021-03-02 17:14:32 +0000

    Create Instance Function completed and user data successfully installs apache

--- primary.py
+++ primary.py
@@ -2,6 +2,8 @@ import boto3
 import botocore
 import json
 
+# sg-0b6477708c0e8462d
+
 # AWS API KEYS
 def awsApiKey():
 
@@ -44,37 +46,66 @@ def awsApiKey():
     # THIS CODE WAS RECIEVED FROM THE BOTO3 DOCUMENTATION : https://boto3.amazonaws.com/v1/documentation/api/latest/guide/credentials.html
     global ec2client 
     ec2client = boto3.resource('ec2',aws_access_key_id = accessKeyId,aws_secret_access_key = secretAccessKey)
-    print("We Patched You In!")
+    print("\nWe Patched You In!")
     return ec2client
 
 def createNewInstance():
     # THE FOLLOWING CODE WAS TAKEN FROM THE BOTO3 DOCUMENTATION : https://boto.readthedocs.io/en/latest/ref/ec2.html#boto.ec2.connection.EC2Connection.run_instances
-    key_pair_name = input("Please Enter a Name for your Key : ")
-    # instance_name = input("Please Enter a Name for your Instance : ")
-    try :
-        ec2client.create_key_pair(KeyName = key_pair_name)
-    except botocore.exceptions.ClientError :
+
+    while True:
+        try :
+            key_pair_name = input("\nPlease Enter a Name for your Key : ")
+            key_file = "./%s.pem" % (key_pair_name)
+            new_key_pair = ec2client.create_key_pair(KeyName = key_pair_name)
+            with open(key_file, 'w') as file:
+                file.write(new_key_pair.key_material)
+            break
+        except botocore.exceptions.ClientError :
+            pass
         print("\n Key Already Exists \n")
-    print("Pending......")
-    user_data = '''#!/bin/bash
-    sudo yum install httpd -y
-    sudo systemctl enable httpd
-    sudo systemctl start httpd'''
+
+    user_data = '''
+    #!/bin/bash
+    yum update -y
+    yum install httpd -y
+    systemctl enable httpd
+    systemctl start httpd
+    '''
+
+    while True:
+        try:
+            user_security_group_id = input("Please Enter a Security Group Id: ")
+            currentInstance = ec2client.create_instances(
+                ImageId='ami-096f43ef67d75e998',
+                MinCount=1,
+                MaxCount=1,
+                InstanceType='t2.nano',
+                KeyName=key_pair_name,
+                SecurityGroupIds=[user_security_group_id],
+                UserData=user_data)
+            break
+        except botocore.exceptions.ClientError:
+            pass
+        print("\nYour Security Key is invalid\n")
+
+    for inst in ec2client.instances.all():
+        if inst.id == currentInstance[0].id:
+            print("\nPending......\n")
+            inst.wait_until_running()
+            inst.reload()
+            public_ip = inst.public_ip_address
     
-        
-    currentInstance = ec2client.create_instances(ImageId='ami-096f43ef67d75e998', MinCount=1, MaxCount=1, InstanceType='t2.nano',KeyName=key_pair_name,UserData = user_data)
-    currentInstance.create_security_group(GroupName = 'HTTP',Description='ALLOWING ACCESS THROUGH THE INTERNET')
-    print("Your Instance Has Been Created!")
+    print("\nYour Instance Has Been Created!\n\nYou can Visit it Here : http://%s\n" % (public_ip))
     
 
 
 def mainMenu():
 
-    print("Main Menu")
+    print("\nMain Menu")
     print("---------")
     print("1. Create A New Instance")
     print("2. Start/Stop an Instance")
-    print("3. Upload an Image")
+    print("3. Create Bucket")
     print("4. Upload an Image")
     print("---------")
     userSelection = input("Your Selection : ")
